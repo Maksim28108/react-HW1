b8ba8340dfff8fe5393dfbe10854aab757fc67db
Maksim Bilukha <bilukha.maksim@student.ehu.lt>
2026-01-14 01:28:41 +0200

    HW5 (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import { useState } from "react";  
+// import { useState } from "react";  
+// import './App.css'
+// import MenuPage from "./components/Menu_tmp/Menu";
+// import Header from "./components/header/Header"
+// import Footer from "./components/footer/Footer"
+
+
+// export default function App() {
+//   const [cartCount, setCartCount] = useState(0);
+//   const [cartTotal, setCartTotal] = useState(0);
+
+//   function handleAddToCart(price, qty) {
+//     const q = Math.max(1, Number(qty) || 1);
+//     setCartCount(c => c + q);
+//     setCartTotal(t => t + price * q);
+//   }
+
+//   return (
+//   <>
+//     <Header count={cartCount} total={cartTotal}/>
+//     <MenuPage onAddToCart={handleAddToCart}  />
+//     <Footer />
+//   </> 
+//   )
+// }
+
 import './App.css'
-import MenuPage from "./components/Menu_tmp/Menu";
-import Header from "./components/header/Header"
-import Footer from "./components/footer/Footer"
+import { MainPage } from './pages/mainpage/MainPage'
 
-
-export default function App() {
-  const [cartCount, setCartCount] = useState(0);
-  const [cartTotal, setCartTotal] = useState(0);
-
-  function handleAddToCart(price, qty) {
-    const q = Math.max(1, Number(qty) || 1);
-    setCartCount(c => c + q);
-    setCartTotal(t => t + price * q);
-  }
-
+export function App(){
   return (
-  <>
-    <Header count={cartCount} total={cartTotal}/>
-    <MenuPage onAddToCart={handleAddToCart}  />
-    <Footer />
-  </> 
+    <MainPage />
   )
 }
